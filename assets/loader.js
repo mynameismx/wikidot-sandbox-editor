76e27717444282b06e75fef1bcdd1ec1a51d502a
mynameismx <kirill.petrov.ynt@gmail.com>
2025-02-16 17:23:08 +0900

    Delete assets/loader.js

--- assets/loader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", ()=>{
-        var s = document.createElement('script');
-        s.setAttribute('type', 'text/javascript');
-        s.setAttribute('src', chrome.runtime.getURL('/js/inject.js'));
-        document.body.appendChild(s);
-}, false);
